56f9bc0d004f421d7a810dee2783861da7861e51
misawa2048 <misawa@elix.jp>
2021-03-20 21:28:37 +0900

    PボタンでBGM再生停止

---
+++ Assets/YahooHackDay2021/OnlyEditorDemo/BGMCtrlByButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMCtrlByButton : MonoBehaviour
+{
+    [SerializeField] AudioSource m_ac = null;
+    [SerializeField] KeyCode m_key = KeyCode.P;
+    bool m_isPlayed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (m_ac == null)
+        {
+            m_isPlayed = false;
+            m_ac = GetComponent<AudioSource>();
+        }        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(m_key))
+        {
+            if (m_isPlayed)
+            {
+                m_ac.Stop();
+            }
+            else
+            {
+                m_ac.Play();
+            }
+            m_isPlayed = !m_isPlayed;
+        }        
+    }
+}
